3d450ea94e28e78e36585171c51e04e74ea2b261
haloa19 <hj199514@gmail.com>
2022-01-17 23:23:03 +0900

    충돌 테스트

--- src/chap04/Exercise07.java
+++ src/chap04/Exercise07.java
@@ -11,9 +11,9 @@ public class Exercise07 {
 		Scanner scanner = new Scanner(System.in);
 		
 		while(run) {
-			System.out.println("-----------------------------");
+			System.out.println("------------------------------");
 			System.out.println("1.예금 | 2.출금 | 3.잔고 | 4.종료");
-			System.out.println("-----------------------------");
+			System.out.println("------------------------------");
 			System.out.print("선택> ");
 			
 			String selectNum = scanner.next();
