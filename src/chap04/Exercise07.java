1a5898b3576135c580f1b468d3295e041f12c38e
haloa19 <hj199514@gmail.com>
2022-01-17 22:53:14 +0900

    조건문반복문 문제

---
+++ src/chap04/Exercise07.java
@@ -0,0 +1,49 @@
+package chap04;
+
+import java.util.Scanner;
+
+public class Exercise07 {
+
+	public static void main(String[] args) {
+		boolean run = true;
+		int balance = 0;
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		while(run) {
+			System.out.println("-----------------------------");
+			System.out.println("1.예금 | 2.출금 | 3.잔고 | 4.종료");
+			System.out.println("-----------------------------");
+			System.out.print("선택> ");
+			
+			String selectNum = scanner.next();
+			int money = 0;
+			
+			switch(selectNum) {
+			case "1":
+				System.out.print("예금액> ");
+				money = scanner.nextInt();
+				balance += money;
+				break;
+			case "2":
+				System.out.print("출금액> ");
+				money = scanner.nextInt();
+				balance -= money;
+				break;
+			case "3":
+				System.out.println("잔고> " + balance);
+				break;
+			case "4":
+				run = false;
+				break;
+			default:
+				System.out.println("번호를 선택해주세요.");
+			}
+			
+		}
+		scanner.close();
+		System.out.println("프로그램 종료");
+
+	}
+
+}
